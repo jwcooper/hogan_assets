cae77e9124e3ae3b12d1b22c259c663be5daea77
Justin Cooper <jwcooper@gmail.com>
2016-03-08 13:45:14 -0600

    Update for latest sprockets

--- lib/hogan_assets.rb
+++ lib/hogan_assets.rb
@@ -2,18 +2,14 @@ require 'hogan_assets/version'
 require 'hogan_assets/config'
 
 module HoganAssets
-  extend Config
-
-  autoload :Hogan, 'hogan_assets/hogan'
-  autoload :Tilt, 'hogan_assets/tilt'
-
-  if defined? Rails
-    require 'hogan_assets/engine'
-  else
-    require 'sprockets'
-    Config.load_yml! if Config.yml_exists?
-    Config.template_extensions.each do |ext|
-      Sprockets.register_engine ".#{ext}", Tilt
-    end
-  end
+  class Engine < ::Rails::Engine
+    initializer "sprockets.hogan", :group => :all do |app|
+      HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
+      Rails.application.config.assets.configure do |env|
+        HoganAssets::Config.template_extensions.each do |ext|
+          env.register_engine(".#{ext}", Tilt)
+        end
+      end 
+    end 
+  end 
 end
