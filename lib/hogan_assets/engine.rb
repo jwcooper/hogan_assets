b6002a56461703292b019fc12a88aec2fc753867
Justin Cooper <jwcooper@gmail.com>
2017-06-05 11:12:53 -0500

    silence deprecation warnings, first attempt at register_transformer

--- lib/hogan_assets/engine.rb
+++ lib/hogan_assets/engine.rb
@@ -4,9 +4,16 @@ module HoganAssets
       HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
       Rails.application.config.assets.configure do |env|
         HoganAssets::Config.template_extensions.each do |ext|
-          env.register_engine(".#{ext}", Tilt)
+          if env.respond_to?(:register_transformer)
+            env.register_mime_type 'text/hogan', extensions: [".#{ext}"], charset: :unicode
+            env.register_transformer 'text/hogan', 'application/javascript', Tilt
+          end
+
+          if env.respond_to?(:register_engine)
+            env.register_engine ".#{ext}", Tilt, silence_deprecation: true
+          end
         end
-      end 
-    end 
-  end 
+      end
+    end
+  end
 end
